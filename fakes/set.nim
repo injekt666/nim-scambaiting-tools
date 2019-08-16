4b910fbdff25be8c55f3dea9e01cde9d866ff51a
Juan Carlos <juancarlospaco@gmail.com>
2019-08-16 09:43:12 -0300

    Init

---
+++ fakes/set.nim
@@ -0,0 +1,39 @@
+echo """
+ALLUSERSPROFILE=C:\ProgramData
+APPDATA=C:\Users\user\AppData\Roaming
+CommonProgramFiles=C:\Program Files\Common Files
+CommonProgramFiles(x86)=C:\Program Files (x86)\Common Files
+CommonProgramW6432=C:\Program Files\Common Files
+ComSpec=C:\Windows\system32\cmd.exe
+FPS_BROWSER_APP_PROFILE_STRING=Internet Explorer
+FPS_BROWSER_USER_PROFILE_STRING=Default
+HOMEDRIVE=C:
+HOMEPATH=\Users\user
+LOCALAPPDATA=C:\Users\user\AppData\Local
+NUMBER_OF_PROCESSORS=1
+OS=Windows_NT
+Path=C:\Program Files\Python37\Scripts\;C:\Program Files\Python37\;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Program Files\Git\cmd;C:\Users\user\AppData\Local\Microsoft\WindowsApps;;C:\Users\user\nim\nim-0.20.2\bin;C:\Users\user\.nimble\bin
+PATHEXT=.COM;.EXE;.BAT;.CMD;.VBS;.VBE;.JS;.JSE;.WSF;.WSH;.MSC
+PROCESSOR_ARCHITECTURE=AMD64
+PROCESSOR_IDENTIFIER=AMD64 Family 21 Model 2 Stepping 0, AuthenticAMD
+PROCESSOR_LEVEL=21
+PROCESSOR_REVISION=0200
+ProgramData=C:\ProgramData
+ProgramFiles=C:\Program Files
+ProgramFiles(x86)=C:\Program Files (x86)
+ProgramW6432=C:\Program Files
+PROMPT=$P$G
+PSModulePath=C:\Program Files\WindowsPowerShell\Modules;C:\Windows\system32\WindowsPowerShell\v1.0\Modules
+PUBLIC=C:\Users\Public
+SESSIONNAME=Console
+SystemDrive=C:
+SystemRoot=C:\Windows
+TEMP=C:\Users\user\AppData\Local\Temp
+TMP=C:\Users\user\AppData\Local\Temp
+USERDOMAIN=DESKTOP-9GEF6VP
+USERDOMAIN_ROAMINGPROFILE=DESKTOP-9GEF6VP
+USERNAME=user
+USERPROFILE=C:\Users\user
+windir=C:\Windows
+
+"""
