d12a585d6304f84e23f85eff48c766960185b445
Juan Carlos <juancarlospaco@gmail.com>
2019-08-16 22:58:11 -0300

    Init

---
+++ fakes/manage_bde.nim
@@ -0,0 +1,7 @@
+echo """
+BitLocker Drive Encryption: Configuration Tool version 10.0.14393
+Copyright (C) 2019 Microsoft Corporation. All rights reserved.
+
+ERROR: An error occurred (code 0x80070422):
+The service cannot be started, either because it is disabled or because it has no enabled devices associated with it.
+"""
